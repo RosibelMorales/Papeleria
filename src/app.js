b91abcc8144e20bcf0cf1003a223484ddf744f29
Rosibel Morales <rossibel8217@gmail.com>
2022-12-07 17:27:21 -0600

    Añadiendo cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 
@@ -7,12 +8,13 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 
 //Middlewares
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended:false}));
 app.use('/api/clientes', require('./routes/clientes.routes'));
 app.use('/api/admin', require('./routes/administrador.routes'));
-app.use('/api/repartidor', require('./routes/repartidor.routes'));
-app.use('/api/producto', require('./routes/producto.routes'));
+app.use('/api/repartidores', require('./routes/repartidor.routes'));
+app.use('/api/productos', require('./routes/producto.routes'));
 
 module.exports = app
