035361554d4a6ad8d774c923a7942d33d41ab7b1
Rosibel Morales <rossibel8217@gmail.com>
2022-12-07 16:05:17 -0600

    root

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.set('port', process.env.PORT || 3000)
 //Middlewares
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.urlencoded({extended:false}))
-app.use('/api/', require('./routes/index.routes'))
+app.use(express.urlencoded({extended:false}));
+app.use('/api/', require('./routes/index.routes'));
 app.use('/api/clientes', require('./routes/clientes.routes'));
 app.use('/api/admin', require('./routes/administrador.routes'));
 app.use('/api/repartidor', require('./routes/repartidor.routes'));
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,8 +1,8 @@
 const {pool} = require("../database.js")
 
-const root = async(req,res)=>{
+const root = async (req, res) => {
     const [rows]=await pool.query('SELECT * FROM Producto');
     res.json(rows);
-};
+}
 
 module.exports = root
