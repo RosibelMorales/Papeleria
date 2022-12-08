d19b91c242d6de5708168906f777009b3431de02
Rosibel Morales <rossibel8217@gmail.com>
2022-12-08 11:55:40 -0600

    Bug recuperación de fecha

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -14,7 +14,7 @@ controladorProducto.obtenerProducto = async(req,res)=>{
 
 controladorProducto.insertarProducto = async (req, res)=>{
     const {idProducto,CodigoBarras,Categoria,Marca,Descripcion,Piezas,Color,Imagen,Imagen2,UnidadesMayoreo,ExistenciasPaquete,ExistenciasUnidad,Entradas,PrecioUnidad,PrecioPaquete,PrecioMayoreo,CompraPaquete} = req.body
-    const fecha = current_timestamp()
+    const fecha = new Date()
     const [rows] = await pool.query('INSERT INTO Producto VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [idProducto,CodigoBarras,Categoria,Marca,Descripcion,Piezas,Color,Imagen,Imagen2,UnidadesMayoreo,ExistenciasPaquete,ExistenciasUnidad,Entradas,PrecioUnidad,PrecioPaquete,PrecioMayoreo,CompraPaquete,fecha])
     res.json({"status":"Producto insertado exitosamente"})
 }
