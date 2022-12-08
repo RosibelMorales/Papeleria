0719bd582f6ba826ad9394f04e9e0d0d5345d31a
Rosibel Morales <rossibel8217@gmail.com>
2022-12-08 16:27:35 -0600

    Buscar por categoría

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -15,7 +15,7 @@ controladorProducto.obtenerProducto = async(req,res)=>{
 controladorProducto.insertarProducto = async (req, res)=>{
     const {idProducto,CodigoBarras,Categoria,Marca,Descripcion,Piezas,Color,Imagen,Imagen2,UnidadesMayoreo,ExistenciasPaquete,ExistenciasUnidad,Entradas,PrecioUnidad,PrecioPaquete,PrecioMayoreo,CompraPaquete} = req.body
     const fecha = new Date().toLocaleString()
-    const [rows] = await pool.query('INSERT INTO Producto VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [idProducto,CodigoBarras,Categoria,Marca,Descripcion,Piezas,Color,Imagen,Imagen2,UnidadesMayoreo,ExistenciasPaquete,ExistenciasUnidad,Entradas,PrecioUnidad,PrecioPaquete,PrecioMayoreo,CompraPaquete,fecha])
+    const [rows] = await pool.query('INSERT INTO Producto VALUES(Aut,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [idProducto,CodigoBarras,Categoria,Marca,Descripcion,Piezas,Color,Imagen,Imagen2,UnidadesMayoreo,ExistenciasPaquete,ExistenciasUnidad,Entradas,PrecioUnidad,PrecioPaquete,PrecioMayoreo,CompraPaquete,fecha])
     res.json({"status":"Producto insertado exitosamente"})
 }
 
@@ -32,6 +32,12 @@ controladorProducto.eliminarProducto = async (req, res)=>{
     const idProducto = req.params.idProducto
     const [row] =await pool.query('DELETE FROM Producto WHERE idProducto=?',[idProducto]);
     res.json('{"status":"Repartidor eliminado"')
+}
+
+controladorProducto.buscarCategoria = async(req,res)=>{
+    const categoria = req.params.Categoria
+    const [rows] = await pool.query('SELECT * FROM Producto WHERE Categoria=?', [Categoria])
+    res.send(rows)
 }
 
 module.exports = controladorProducto
